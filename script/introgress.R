5440e9cb435109fdec7b2e2272d013f3a4516622
Tyler DeVos <78694098+tylerdevos@users.noreply.github.com>
2022-03-05 14:39:41 -0500

    Update introgress.R

--- script/introgress.R
+++ script/introgress.R
@@ -30,23 +30,23 @@ triangle.plot(hi.index=hi.index.sim, int.het=int.het.sim, pdf=FALSE)
 #nicer triangle plot with ggplot
 triangledata <- hi.index.sim
 triangledata$het <- int.het.sim
-triangledata$pop <- c("porcatus", "porcatus", "porcatus", "porcatus", "porcatus", "porcatus", "porcatus", "porcatus", "porcatus", "porcatus", "porcatus", "porcatus", "porcatus", "porcatus", "porcatus", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid", "hybrid","hybrid","hybrid","hybrid","hybrid","hybrid","carolinensis", "carolinensis", "carolinensis", "carolinensis", "carolinensis", "carolinensis", "carolinensis", "carolinensis", "carolinensis", "carolinensis", "carolinensis", "carolinensis", "carolinensis", "carolinensis", "carolinensis", "carolinensis")
+triangledata$pop <- c("porcatus", "porcatus", "porcatus", "porcatus", "porcatus", "porcatus", "porcatus", "porcatus", "porcatus", "porcatus", "porcatus", "porcatus", "porcatus", "porcatus", "porcatus", "hybrid1", "hybrid1", "hybrid1", "hybrid1", "hybrid1", "hybrid1", "hybrid1", "hybrid2", "hybrid2", "hybrid1", "hybrid1", "hybrid2", "hybrid2", "hybrid2", "hybrid1", "hybrid2", "hybrid1", "hybrid1", "hybrid1", "hybrid1", "hybrid2", "hybrid1", "hybrid2", "hybrid2", "hybrid1", "hybrid1", "hybrid1", "hybrid1", "hybrid1", "hybrid2", "hybrid1", "hybrid1", "hybrid1", "hybrid1", "hybrid2", "hybrid2", "hybrid2", "hybrid2", "hybrid1", "hybrid1", "hybrid2", "hybrid2", "hybrid2", "hybrid2", "hybrid2", "hybrid2", "hybrid2", "hybrid2", "hybrid2", "hybrid2", "hybrid2", "hybrid2", "hybrid2", "hybrid2", "hybrid2", "hybrid1", "hybrid2", "hybrid2", "hybrid2", "hybrid2", "hybrid2", "hybrid2", "hybrid2", "hybrid2", "hybrid1", "hybrid1", "hybrid1", "hybrid2", "hybrid1", "carolinensis", "carolinensis", "carolinensis", "carolinensis", "carolinensis", "carolinensis", "carolinensis", "carolinensis", "carolinensis", "carolinensis", "carolinensis", "carolinensis", "carolinensis", "carolinensis", "carolinensis", "carolinensis")
 ggplot(triangledata, aes(h,het,color=pop,fill=pop))+
-  geom_point(shape=21, alpha=0.75, size=3, color="black")+
+  geom_point(shape=21, alpha=1, size=3, color="black")+
   geom_segment(x=0,y=0,xend=0.5,yend=1,color="black",size=0.01)+
   geom_segment(x=0,y=0,xend=1,yend=0,color="black",size=0.01)+
   geom_segment(x=1,y=0,xend=0.5,yend=1,color="black",size=0.01)+
   labs(fill="Group")+
   scale_x_continuous(name="Hybrid Index", limits=c(0,1), breaks=seq(0,1, by=0.1))+
   scale_y_continuous(name="Interspecific Heterozygosity", limits=c(0,1), breaks=seq(0,1, by=0.1))+
-  scale_fill_manual(values=c("gold","mediumpurple2","seagreen3"))+
+  scale_fill_manual(labels = c(expression(italic("A. carolinensis")),"hybrid group 1","hybrid group 2",expression(italic("A. porcatus"))), values=c("#D5A034","#B4CCA4","#749D58","#437570"))+
   theme_bw()+
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),legend.title.align = 0.45, legend.text.align=0)
 
 #make marker ancestry plot to visualize patterns of introgression
 mk.image(introgress.data=count.matrix.1, loci.data=loci.data.sim, 
          hi.index=hi.index.sim, ylab.image="Individuals", main.image="Marker Ancestry Plot",
-         xlab.h="carolinensis ancestry", pdf=FALSE)
+         xlab.h="A. carolinensis ancestry", pdf=FALSE)
 
 #ancestry plot source code with custom modifications
 introgress.data <- count.matrix.1
@@ -68,7 +68,7 @@ for(i in 1:length(lg)){
   marker.n[i] <- sum(loci.data[,3]==lg[i])}
 loci.touse<-1:(dim(count.matrix)[1])
 ind.touse<-1:(dim(count.matrix)[2])
-col.image<-c("#A1D99B", "#41AB5D", "#005A32")
+col.image<-c("#D5A034", "#749D58", "#437570")
 marker.order<-loci.touse
 matrix <- count.matrix[marker.order, order(hi.index[ind.touse])]
     
@@ -90,5 +90,5 @@ abline(v=7261.5, col="black", lwd=3)
 abline(v=7311.5, col="black", lwd=3)
 axis(1, at=c(1079.5,2893,4356.5,5638,6570,7105.5,7287,8791), labels=c("chr.1","chr.2","chr.3","chr.4","chr.5","chr.6","*","unassigned scaffolds"), tick=FALSE, cex=0.5)
 axis(1, at=c(0.5,2157.5,3627.5,5084.5,6190.5,6948.5,7261.5,7272.5,7280.5,7289.5,7310.5,7311.5,10269),labels=c("","","","","","","","","","","","",""),line=0.5)
-axis(2, at=c(8,50,92.5), labels=c("porcatus\n(n=15)","hybrids\n(n=69)","carolinensis\n(n=16)"), tick=FALSE)
+axis(2, at=c(8,50,92.5), labels=c("A. porcatus\n(n=15)","hybrids\n(n=69)","A. carolinensis\n(n=16)"), tick=FALSE)
 axis(2, at=c(0.5,15.5,84.5,100.5), labels=c("","","",""),line=0.5)
