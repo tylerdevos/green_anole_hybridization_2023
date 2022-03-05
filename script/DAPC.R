760e0a1c47e7e529a752447f7bdc9853a02c5776
Tyler DeVos <78694098+tylerdevos@users.noreply.github.com>
2022-03-05 14:19:23 -0500

    Update DAPC.R

--- script/DAPC.R
+++ script/DAPC.R
@@ -29,7 +29,7 @@ temp <- optim.a.score(dapc1)
 #Plot groups
 myCol <- c("#D5A034","#B4CCA4","#749D58","#437570")
 scatter(dapc1, clab=0, col=myCol, scree.da=TRUE)
-scatter(dapc1, 1,1, clab=0, col=myCol, scree.da=FALSE, leg=FALSE, txt.leg=c("A. carolinensis","hybrid 1","hybrid 2","A. porcatus"))
+scatter(dapc1, 1,1, clab=0, col=myCol, scree.da=FALSE, leg=FALSE, txt.leg=c(italic("A. carolinensis"),"hybrid 1","hybrid 2",italic("A. porcatus")))
 scatter(dapc1, 2,2, clab=0, col=myCol, scree.da=FALSE, leg=FALSE, posi.leg=("topleft"),txt.leg=c("A. carolinensis","hybrid 1","hybrid 2","A. porcatus"))
 
 #Investigate admixed individuals
